3640ffe8ed9f411a98e072333eba6ff7a0365c7e
unknown <havrdaj429@gmail.com>
2023-12-10 12:27:56 +0100

    final touches

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const findOrCreate = require("mongoose-findorcreate");
@@ -14,6 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/public', express.static('public'));
 app.use(passport.initialize());
+
+app.use(session({
+  secret: "SHESAIDSHEWASTWELVE",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.session());
 
 mongoose.connect('mongodb://127.0.0.1:27017/personalDB');
 
@@ -39,6 +47,28 @@ userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+//serialize
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+    });
+  });
+});
+//deserialize
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+
+//SETUP DONE//
+
 
 
 app.route("/").get(function(req, res) {
@@ -76,10 +106,75 @@ Post.find().then(function(posts){
 })
 
 app.get("/admin", function(req, res){
-  res.render("admin.ejs", {})
+  res.render("admin.ejs", {response: " "})
+});
+
+var isLogged;
+
+ app.route("/register").post(function(req,res){
+   console.log(req.body["username"]);
+   User.register({username: req.body["username"], active: true}, req.body.password).then(function (err, user) {
+     if(!err){
+         console.log(err);
+         res.redirect("/");
+     }else{
+         passport.authenticate("local") (req, res, function(){
+                 res.redirect("/admin")    
+         })  
+     }
+     });
+ });
+// 6572ce0405e770b0892bef5d
+
+app.post("/login", function(req,res){ 
+    const username = req.body.username;
+    const password = req.body.password;
+
+  User.findOne({username: username}).then(function(foundUser){
+    console.log(foundUser);
+    if(foundUser === null){
+      res.render("admin.ejs", {response: "User does not exist!"});
+    }else{
+      isLogged = true;
+      res.redirect("/admin/posts");
+    }
+    
+  });
+});
+
+app.get("/logout", function(req,res) {
+  req.logout(function (err) {
+      if(err){
+          console.log(err);
+      }else{
+          isLogged = false;
+          console.log(isLogged);
+          res.redirect("/");
+        }
+  });
+
 })
+
+app.get("/admin/posts", function(req,res){
+    console.log(isLogged);
+    if(isLogged === true){
+      Post.find().then(function(posts){
+        res.render("admin-posts.ejs", {posts: posts});
+      });
+    }else{
+      res.redirect("/admin");
+    };
+    
+});
+
+app.post("/delete", function(req,res){
+  const deletedPost = req.body.delete;
+  Post.findByIdAndDelete({_id: deletedPost}).then(function(){
+    res.redirect("/admin/posts");
+  });
+});
 
 
 app.listen("3000", function() {
     console.log("Server is running on PORT 3000.")
-})
+});
